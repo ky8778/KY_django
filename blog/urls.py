58b252ed7d9fd426244fe92b6f258f9264e0dc4b
ky8778 <gunyoung8778@gmail.com>
2021-03-26 23:53:18 +0900

    [CBV] Post List page updated

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),  # blog/ url에서 import한 views.py에 있는 index() 함수를 실행한다.
+    # [FBV] blog/ url에서 import한 views.py에 있는 index() 함수를 실행한다.
+    # path('', views.index),
+    # [CBV] ListView를 사용할 때는 '_list' 가 붙은 html 파일을 기본 템플릿으로 사용하도록 되어있다.
+    # 방법1. PostList Class에서 template_name을 직접 지정하는 방법
+    # 방법2. post_list.html을 만드는 방법
+    path('', views.PostList.as_view()),
     path('<int:pk>/', views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
+# CBV
+class PostList(ListView):
+    model = Post
+    # 방법1
+    template_name = 'blog/index.html'
+    ordering = '-pk'
+
+''' FBV
 def index(request):
     # posts = Post.objects.all()      # Query로 데이터를 가져오는 방법
     posts = Post.objects.all().order_by('-pk')      # pk 값의 역순 (생성 기준 최신 글 부터 보여줌)
@@ -11,6 +20,7 @@ def index(request):
     }
 
     return render(request, 'blog/index.html', my_posts)   # index.html 파일 렌더링
+'''
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
