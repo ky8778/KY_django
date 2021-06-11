009b6125b97231f9d8c68aaac0041dfd9e75a77a
ky8778 <gunyoung8778@gmail.com>
2021-06-11 20:58:04 +0900

    [blog] Post model timefield auto_now updated

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,5 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
