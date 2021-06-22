abf7d2400d79488693678d30c62e3f4e80f1750d
ky8778 <gunyoung8778@gmail.com>
2021-06-22 21:54:05 +0900

    [blog] single_post_page

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,10 @@ def index(request):
         'posts': posts,
     }
     return render(request, 'blog/index.html', context)
+    
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/single_post_page.html', context)
