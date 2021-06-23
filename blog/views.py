fe3571be90d39de6101e968c043348075e2f17af
ky8778 <gunyoung8778@gmail.com>
2021-06-23 22:23:16 +0900

    [Post List] CBV로 전환

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
+''' CBV '''
+class PostList(ListView):
+    model = Post
+    # default template : <app_label>/<model_name>_list.html
+    template_name = 'blog/index.html' 
+	# default context name : object_list
+    context_object_name = 'posts'
+    ordering = '-pk'
+
+''' FBV
 def index(request):
     posts = Post.objects.all().order_by('-pk')
     context = {
         'posts': posts,
     }
     return render(request, 'blog/index.html', context)
+'''
     
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
