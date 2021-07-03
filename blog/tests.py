6b1f34c69c1c0d6b4d5f77dacc618cc98554cc9d
ky8778 <gunyoung8778@gmail.com>
2021-07-03 20:48:00 +0900

    [Blog] TDD - Category Page Test updated

--- blog/tests.py
+++ blog/tests.py
@@ -140,3 +140,19 @@ class TestView(TestCase):
 
         # 6. author 확인
         self.assertIn(self.user_one.username.upper(), main_area.text)
+    
+    def test_category_page(self):
+        response = self.client.get(self.category_game.get_absolute_url())
+        self.aseertEqaul(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.navbar_test(soup)
+        self.category_card_test(soup)
+
+        self.assertIn(self.category_game.name, soup.h1.text)
+
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(self.category_game.name, main_area.text)
+        self.assertIn(self.post_001.title, main_area.text)
+        self.assertNotIn(self.post_002.title, main_area.text)
+        self.assertNotIn(self.post_003.title, main_area.text)
