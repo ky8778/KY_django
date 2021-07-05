373e9fbf2c8c0697d89e255acec701c34572d5ea
ky8778 <gunyoung8778@gmail.com>
2021-07-05 16:38:00 +0900

    [Blog] Login 한 상태에서만 Post Create 가능하도록 변경

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category, Tag
@@ -42,6 +42,15 @@ class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
     # default template name : blog/post_form.html
+
+    # default form_valid 재정의, login한 author 없으면 redirect
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
 
 
 ''' FBV '''
