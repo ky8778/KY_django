f39a752b26e59932cc9e12f9ff9da9ca5b1d27f8
ky8778 <gunyoung8778@gmail.com>
2021-07-07 23:51:53 +0900

    [Blog] Model Comment updated

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from markdownx.models import MarkdownxField
 from markdownx.utils import markdown
 import os
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -18,6 +19,7 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories' # django default 로 Categorys 로 되어있어서 변경해주기
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -27,6 +29,7 @@ class Tag(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/tag/{self.slug}/'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -60,3 +63,15 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
