7e543b9b9c88e1ae72a1c5364c8cd6fafb1ac64c
ky8778 <gunyoung8778@gmail.com>
2021-07-23 23:09:24 +0900

    [Home] Homepage updated

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    recent_posts = Post.objects.order_by('-pk')[:3]
+    context = {
+        'recent_posts': recent_posts,
+    }
+    return render(request, 'home/home.html', context)
 
 def about_me(request):
     return render(request, 'home/about_me.html')
